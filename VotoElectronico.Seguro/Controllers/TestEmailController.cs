81bcf8e7735732aeb9c4054bd81a92c9eb18ed6c
Patricio Quiguango <patricioquiguango.08@gmail.com>
2026-02-03 11:38:04 -0500

    Email controller

---
+++ VotoElectronico.Seguro/Controllers/TestEmailController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotoElectronico.Seguro.Controllers
+{
+    [ApiController]
+    [Route("api/test-email")]
+    public class TestEmailController : ControllerBase
+    {
+        private readonly IEmailSender _emailSender;
+
+        public TestEmailController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendTestEmail()
+        {
+            await _emailSender.SendEmailAsync(
+                "patricioquiguango.08@gmail.com",
+                "Prueba de correo",
+                "<h2>EmailSender funcionando correctamente 🚀</h2>"
+            );
+
+            return Ok("Correo enviado (revisa tu inbox o spam)");
+        }
+    }
+}
